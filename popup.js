52dfefef43d489472c857c4392643a91cf138150
Jakob Taankvist <jakob_taankvist@hotmail.com>
2017-05-17 14:32:53 +0800

    First version, already working

---
+++ popup.js
@@ -0,0 +1,27 @@
+
+function renderStatus(statusText) {
+  document.getElementById('status').innerHTML = statusText;
+}
+
+chrome.tabs.query({'active': true}, function(tabs) {
+    var tab = tabs[0];
+
+    var url = tab.url;
+    var testRequest = new Request("http://content.aub.aau.dk/zorac/zoracjson.php?&url=" + url);
+    renderStatus('Looking up');
+    fetch(testRequest).then(function (response) {
+        return response.json();
+    })
+    .then(function (body) {
+        console.log(body.proxy);
+        if(body.proxy == 1){
+            renderStatus("Redirecting");
+            var newurl = 'http://zorac.aub.aau.dk/login?url=' + url;
+            chrome.tabs.update(tab.id, {url: newurl});
+        } else {
+            renderStatus("No access");
+        }
+    }, function (error) {
+        renderStatus("Error!<br />You are probably not on a http page.");
+    });
+});
